8a342225797c58cf26923a930579c13ad722f32a
dipu <deepshrestha365@gmail.com>
2025-07-02 11:32:49 +0545

    day 32 assingment for form validation

--- assignments/day32/script.js
+++ assignments/day32/script.js
@@ -8,34 +8,98 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// Fetch api function
-async function fetchApi(api) {
-  try {
-    const response = await fetch(api);
-
-    if (!response.ok) {
-      throw new Error(`HTTP eror! status: ${response.status}`);
-    }
-
-    let data = response.json();
-    // console.log('resp data', data)
-    return data;
-  } catch (error) {
-    console.error("Fetch Error:", error);
-  }
-}
-// Change to Ttile Case function
-function toTitleCase(str) {
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-}
 // Fetch products api
 const productApi = "https://fakestoreapi.com/products";
 // Fetch users api
 const userApi = "https://fakestoreapi.com/users";
+// Fetch api function
+async function fetchApi(api, method = 'GET', data = null, headers = {}) {
+  try {
+    const options = {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers
+      }
+    };
+    if(data && method !== 'GET'){
+      options.body = JSON.stringify(data);
+    }
+    const response = await fetch(api , options);
+    if (!response.ok) {
+      throw new Error(`HTTP eror! status: ${response.status}`);
+    }
+    const result = response.json();
+    return result;
+  } catch (error) {
+    console.error("Fetch Error:", error);
+    throw error
+  }
+}
+
+
+// form submit
+const formArea = document.querySelector(".form-area");
+const formBtn = formArea.querySelector("button");
+const submitResponse = document.querySelector(".submit-response");
+formArea.addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  const form = event.target;
+  const formData = new FormData(form);
+  const title = formData.get("title");
+  const price = formData.get("price");
+  const description = formData.get("description");
+  const category = formData.get("category");
+  const imageFile = formData.get("image");
+
+  let payLoadData = {
+    id: 0,
+    title: title,
+    price: price,
+    description: description,
+    category: category,
+    image: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg',
+  };
+
+  if(!title || !price || !description || !category){
+    alert("Please enter complete credentials")
+    return
+  }
+  if(isNaN(price)) {
+    alert("Please enter valid price")
+    return
+  }
+
+  fetchApi(productApi, 'POST', payLoadData)
+  .then((response) => {
+    formBtn.classList.add("show");
+  })
+  .then((data) => {
+    console.log("data here", data);
+    if (formData) {
+      setTimeout(() => {
+        const para = document.createElement("p");
+        para.innerText = "Thanks for submitting message";
+        para.style.color = "green";
+        submitResponse.appendChild(para);
+        formBtn.classList.remove("show");
+        console.log("Success:", data);
+        //  alert("Form submitted successfully!");
+        setTimeout(() => {
+          para.remove();
+        }, 2000);
+
+        form.reset();
+      }, 1500);
+    }
+  })
+  .catch((error) => {
+    console.error("Error submitting form:", error);
+    alert("Something went wrong!");
+  });
+
+});
 // Create Card List function
 async function cardListFunc(api) {
   const respData = await fetchApi(api);
@@ -113,6 +177,14 @@ async function tableListFunc(api) {
 }
 tableListFunc(userApi);
 
+// Change to Ttile Case function
+function toTitleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
 // Action (Edit/Delete) Button Function
 function btnDeletefunction(item, index) {
   console.log("delet|ed item", item);
@@ -122,68 +194,3 @@ function btnDeletefunction(item, index) {
 function btnEditfunction() {
   console.log("Edited");
 }
-
-// form submit
-const formArea = document.querySelector(".form-area");
-const formBtn = formArea.querySelector("button");
-const submitResponse = document.querySelector(".submit-response");
-formArea.addEventListener("submit", function (event) {
-  event.preventDefault();
-  formBtn.classList.add("show");
-  const form = event.target;
-  const formData = new FormData(form);
-  const title = formData.get("title");
-  const price = formData.get("price");
-  const description = formData.get("description");
-  const category = formData.get("category");
-  const imageFile = formData.get("image");
-
-  console.log("formData.Json()", formData.json());
-  console.log("imageFile", imageFile);
-
-  let payLoadData = {
-    id: 0,
-    title: title,
-    price: price,
-    description: description,
-    category: category,
-    image: imageFile,
-  };
-
-  const newJsonData = JSON.stringify(payLoadData);
-  console.log("newJsonData:", newJsonData);
-
-  fetch("https://fakestoreapi.com/products", {
-    method: "POST",
-    body: newJsonData,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      response.json();
-    })
-    .then((data) => {
-      console.log("data here", data);
-      if (formData) {
-        setTimeout(() => {
-          const para = document.createElement("p");
-          para.innerText = "Thanks for submitting message";
-          para.style.color = "green";
-          submitResponse.appendChild(para);
-          formBtn.classList.remove("show");
-          console.log("Success:", data);
-          //  alert("Form submitted successfully!");
-          setTimeout(() => {
-            para.remove();
-          }, 2000);
-
-          form.reset();
-        }, 1500);
-      }
-    })
-    .catch((error) => {
-      console.error("Error submitting form:", error);
-      alert("Something went wrong!");
-    });
-});
